69f32422c9948ac9db6321b74f49eff0bd64f482
Jesse Becker <hawson@gmail.com>
2025-05-07 21:55:37 -0400

    refactor/lint checkschedule.sh

--- bin/checkschedule.sh
+++ bin/checkschedule.sh
@@ -11,39 +11,41 @@ cd "$(dirname "$0")"
 
 # load configuration
 if [ -e "config.sh" ]; then
-	source config.sh
+    source ./config.sh
 fi
 
 # load utils
 if [ -e "utils.sh" ]; then
-	source utils.sh
+    source ./utils.sh
 else
-	echo "Could not find utils.sh in `pwd`"
-	exit
+    echo "Could not find utils.sh in `pwd`"
+    exit
 fi
 
 # get minute of day
-CURRENTMINUTE=$(( `date +%-H`*60 + `date +%-M` ))
+HOUR=$(date +%-H)
+MIN=$(date +%-M)
+CURRENTMINUTE=$(( HOUR*60+MIN ))
 
 # SCHEDULE="21:00-24:00=30"
 for schedule in $SCHEDULE; do
-	echo "-------------------------------------------------------"
-	echo "Parsing \"$schedule\""
-	read STARTHOUR STARTMINUTE ENDHOUR ENDMINUTE INTERVAL << EOF
-		$( echo " $schedule" | sed -e 's/[:,=,\,,-]/ /g' -e 's/\([^0-9]\)0\([[:digit:]]\)/\1\2/g' )
+    echo "-------------------------------------------------------"
+    echo "Parsing \"$schedule\""
+    read -r STARTHOUR STARTMINUTE ENDHOUR ENDMINUTE INTERVAL << EOF
+        $( echo " $schedule" | sed -e 's/[:,=,\,,-]/ /g' -e 's/\([^0-9]\)0\([[:digit:]]\)/\1\2/g' )
 EOF
-	echo "	Starts at $STARTHOUR hours and $STARTMINUTE minutes"
-	echo "	Ends at $ENDHOUR hours and $ENDMINUTE minutes"
-	echo "	Interval is $INTERVAL minutes"
+    echo "    Starts at $STARTHOUR hours and $STARTMINUTE minutes"
+    echo "    Ends at $ENDHOUR hours and $ENDMINUTE minutes"
+    echo "    Interval is $INTERVAL minutes"
 
-	START=$(( 60*$STARTHOUR + $STARTMINUTE ))
-	END=$(( 60*$ENDHOUR + $ENDMINUTE ))
+    START=$(( 60*STARTHOUR + STARTMINUTE ))
+    END=$(( 60*ENDHOUR + ENDMINUTE ))
 
-	if [ $END -lt $START ]; then
-		echo "!!!!!!! End time is before start time."
-	fi
+    if [ $END -lt $START ]; then
+        echo "!!!!!!! End time is before start time."
+    fi
 
-	if [ $CURRENTMINUTE -ge $START ] && [ $CURRENTMINUTE -lt $END ]; then
-		echo "    --> This is the active setting"
-	fi
+    if [ $CURRENTMINUTE -ge $START ] && [ $CURRENTMINUTE -lt $END ]; then
+        echo "    --> This is the active setting"
+    fi
 done
